fc2705f0a605d60f91b06c39ef282d6d76585516
moo7md <moo7md3@gmail.com>
2019-08-19 22:25:17 -0400

    fixed script tag. added doctype support and fixed some bugs

--- hlover.py
+++ hlover.py
@@ -9,6 +9,68 @@
 import os.path
 import sys
 
+# constants for !doctype
+doctypes = {"html5": '<!DOCTYPE html>',
+            "html4s": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+            "html4t": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+            "html4f": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
+            "xhtml1s": '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" '
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> ',
+            "xhtml1t": '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" '
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> ',
+            "xhtml1f": '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" '
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"> ',
+            "xhtml1.1": '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'}
+
+
+def get_string(char, file):
+    # handles strings in JavaScript
+    string = char
+    char = file.read(1)
+    counter = 0 if char == '"' else 1
+    while counter != 0 and char != '':
+        if char == '\\':
+            string += char
+            string += file.read(1)
+        elif char == '"' or '\'':
+            counter -= 1
+            string += char
+            break
+        else:
+            string += char
+        char = file.read(1)
+    else:
+        string += char
+    return string
+
+
+def writeScriptBody(char, file):
+    # only write the body of <script>
+    body = ''
+    counter = 0 if char == '}' else 1
+    while char != '' and counter != 0:
+        if char == '{':
+            body += char
+            counter += 1
+        elif char == '}':
+            if counter == 1:
+                counter -= 1
+            else:
+                body += char
+                counter -= 1
+        elif char == '\'' or char == '"':
+            body += get_string(char, file)
+        else:
+            body += char
+        char = file.read(1)
+    # file.seek(file.tell()-1)
+    if counter > 0:
+        char = body[len(body) - 1]
+        while char == "}" or char == ' ' or char == '\n':
+            body = body[:-1]
+            char = body[len(body) - 1]
+    return body
+
 
 def do_element(file, char, tag):
     result = '<' + tag
@@ -37,6 +99,12 @@ def do_element(file, char, tag):
                 sub_body = do_element(file, char, sub_tag)
                 body += sub_body
                 char = file.read(1)
+            elif char == '\\':
+                body += file.read(1)
+                char = file.read(1)
+            elif tag == 'script':
+                body += writeScriptBody(char, file)
+                break
             else:
                 if char != ']':
                     body += char
@@ -50,20 +118,30 @@ def do_element(file, char, tag):
     return result
 
 
+def is_doctype(token):
+    token = token.lower()
+    return token == 'html5' or token == 'html4s' or token == 'html4t' or token == 'html4f' or token == 'xhtml1s' or token == 'xhtml1t' or token == 'xhtml1f' or token == 'xhtml1.1'
+
+
 def start(file):
     # add <DOCTYPE! HTML> later
     with open(file, 'r') as f:
         char = f.read(1)
-        token = char
+        token = ''
         while char != '':
+            if is_doctype(token):
+                print(doctypes[token])
             if (char == '[') or (char == '{'):
                 # if token.__contains__('p'):
                 new_str = do_element(f, char, token)
                 print(new_str)
                 token = ''
-            char = f.read(1)
-            if (char != ' ') and (char != '[') and (char != '{') and (char != '\n') and (char != '}'):
+            if (char != '[') and (char != '{') and (char != '\n') and (char != '}'):
                 token += char
+            char = f.read(1)
+        # in case doctype was the only thing in the file
+        if is_doctype(token):
+            print(doctypes[token])
 
 
 def main():
@@ -71,8 +149,11 @@ def main():
         print("Error: no input files")
     elif str(sys.argv[1]).endswith('.hl'):
         start_working(sys.argv[1])
-    else:
-        print("Error: unsupported file type")
+    # start_working('/Users/moo7md/PycharmProjects/hlover/hloverSyntax.hl')
+
+
+# else:
+#     print("Error: unsupported file type")
 
 
 def start_working(filename):
